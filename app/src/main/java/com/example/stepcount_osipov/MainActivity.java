2e06116b665439fc5a4551c17edcf5a2aa98ebda
Shizun <Shizun@KULICH>
2025-04-10 19:48:27 +0500

    Программирование окна MainActivity.  Osipov

--- app/src/main/java/com/example/stepcount_osipov/MainActivity.java
+++ app/src/main/java/com/example/stepcount_osipov/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.stepcount_osipov;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +16,40 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Boolean Active = false;
+    SensorManager SensorManager;
+    private int count = 0;
+    private TextView text;
+    private long lastUpdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        SensorManager.registerListener((SensorEventListener) this,
+                SensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        SensorManager.unregisterListener((SensorEventListener) this);
+    }
+
+    public void onStoped(View view){
+        Active = !Active;
+        if (!Active){
+            Button button = findViewById(R.id.bthPause);
+            
+        }
+        else {
+            Button button = findViewById(R.id.bthPause);
+        }
     }
 }
